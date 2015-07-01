21942bf8676079439593e3c94d8a151fb62d4ddf
asj177 <arpitjoshi_177@yahoo.com>
2015-07-01 01:30:29 -0500

    Testing

--- src/main/java/demo/Network.java
+++ src/main/java/demo/Network.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.json.JSONObject;
+
 import com.health.info.CPU;
 import com.health.info.ProcessInfo;
 
@@ -15,55 +17,76 @@ public class Network {
 		try{
 		String processLine=null;
 		String processName="java";
-		String []cmd={"/bin/sh","-c","echo Saibaba@177 | sudo -S dmidecode"};
+		
+		String middle="\"%-8s %-8s %-8s %-8s \\n \"";
+		
+		String first=" | grep \"^ \"";
+		String cc="top -bn 1"+first +"|  awk '{ printf("+
+				
+middle
+				
+				+", $1,$9, $10, $12); }' | head -n 11";
+		
+		
+		String []cmd={"/bin/sh","-c",cc};
 		Process p = Runtime.getRuntime().exec(cmd);
 		InputStream stdin = p.getInputStream();
 		InputStreamReader isr = new InputStreamReader(stdin);
 		BufferedReader br = new BufferedReader(isr);
 
 		String line = null;
-		System.out.println("<OUTPUT>");
-
-		int i=0;
-		boolean applicanceId=false;
-		boolean motherboardname=false;
-		boolean serialNumber=false;
-		while ( (line = br.readLine()) != null){
-			
-			if(line.contains("System Information")){
-				while(!applicanceId || !motherboardname || !serialNumber ){
-					line=br.readLine();
-					String []output=line.split(":");
-					if(line.contains("Serial")){
-						System.out.println("serial number "+output[1]);
-						serialNumber=true;
-					}
-					
-					if(line.contains("UUID")){
-						System.out.println("UUID "+output[1]);
-						applicanceId=true;
-					}
-					
-					if(line.contains("Product")){
-						System.out.println("Product "+output[1]);
-						motherboardname=true;
-					}
-				}
-				
-				
-			}
+		
 		 // System.out.println(line);
-			
-		   
+		int k=0;
+		/*do{
+			line = br.readLine();
+			k++;
+		}while(k<7);*/		   
 		    
 		   
 			
+		while ( (line = br.readLine()) != null){
+			
+			//System.out.println(line);
+			
+			//System.out.println(line.trim());
+			String line2=line.substring(1, line.length()-1);
+			
+			String []splie=line2.split("    ");
+			
+			for(int i=0;i<splie.length;i++){
+			System.out.print(splie[i]+"  ");
+			}
+			
+			System.out.println(" ");
+			/*String []splitLine=line.split(" ");
+			System.out.print("process_name   "+splitLine[splitLine.length-1].trim());
+			
+			System.out.print("  ");
+			
+			System.out.print("process_pid    "+splitLine[0]);
+			
+			System.out.print("  ");
+			
+			System.out.print("uint64_t mem_in_use  "+splitLine[19]);
+			
+			System.out.println(" ");*/
+			/*if(line.contains(name)){
+				JSONObject processInfo=new JSONObject();
+				String []splitLine=line.split(" ");
+				
+				
+				processInfo.put("process_name", name);
+				processInfo.put("process_pid", splitLine[1].trim());
+				processInfo.put("uint64_t mem_in_use", splitLine[19].trim());
+				processArray.put(processInfo);
+				
+				i++;
+			}*/
+			
 		}
 
-		System.out.println("</OUTPUT>");
-
-		
-		System.out.println(processLine);
+	
 
 		}catch(Exception e){
 			e.printStackTrace();
