49f23e9b754b2beb7ebc5e92dd94e026e16214ea
asj177 <arpitjoshi_177@yahoo.com>
2015-07-01 01:30:15 -0500

    Changes for process info

--- src/main/java/com/health/util/Constants.java
+++ src/main/java/com/health/util/Constants.java
@@ -1,9 +1,11 @@
 package com.health.util;
 
+import java.util.ArrayList;
+
 public class Constants {
 
 	public final static int timer=ConfigScheduler.timer;
 	public final static String password=ConfigScheduler.password;
-	public final static String processName=ConfigScheduler.processname;
+	public final static ArrayList<String> processName=ConfigScheduler.processname;
 	
 }
