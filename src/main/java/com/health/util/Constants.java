9617d9cdf0f4b02aa451e6af25a283fee132495c
asj177 <arpitjoshi_177@yahoo.com>
2015-07-04 03:27:16 -0500

    Network CHanges

--- src/main/java/com/health/util/Constants.java
+++ src/main/java/com/health/util/Constants.java
@@ -7,5 +7,6 @@ public class Constants {
 	public final static int timer=ConfigScheduler.timer;
 	public final static String password=ConfigScheduler.password;
 	public final static ArrayList<String> processName=ConfigScheduler.processname;
+	public final static ArrayList<String>networkPorts=ConfigScheduler.ports;
 	
 }
